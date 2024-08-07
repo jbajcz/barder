13b9b6c8a46df73877946169d7cefa730e6a50ad
Maximus <sambucet@msu.edu>
2024-08-07 09:49:18 -0500

    Audio fixed

--- src/audFiles/AudioPlayer.tsx
+++ src/audFiles/AudioPlayer.tsx
@@ -1,18 +1,36 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAudio } from './PlayAudioProvider';
 import styles from './AudioPlayer.module.css';
 
 const AudioPlayer: React.FC = () => {
     const { audio, playlist, currentTrackIndex, transitionToNextTrack } = useAudio();
-
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    
     const togglePlayPause = () => {
         if (audio && audio.paused) {
             audio.play();
+            setIsPlaying(true);
         } else if (audio) {
             audio.pause();
+            setIsPlaying(false);
         }
     };
+
+    useEffect(() => {
+        if (audio) {
+            const handlePlay = () => setIsPlaying(true);
+            const handlePause = () => setIsPlaying(false);
+
+            audio.addEventListener('play', handlePlay);
+            audio.addEventListener('pause', handlePause);
+
+            return () => {
+                audio.removeEventListener('play', handlePlay);
+                audio.removeEventListener('pause', handlePause);
+            };
+        }
+    }, [audio]);
 
     return (
         <div className="text-center bg-black text-white">
@@ -20,7 +38,7 @@ const AudioPlayer: React.FC = () => {
                 Now Playing: {playlist[currentTrackIndex].nickname}
             </p>
             <button onClick={togglePlayPause}>
-                {audio && audio.paused ? 'Play' : 'Pause'}
+                {isPlaying ? 'Pause' : 'Play'}
             </button>
             <button onClick={transitionToNextTrack} className= {styles.buttonSpacing}>
                 Next
